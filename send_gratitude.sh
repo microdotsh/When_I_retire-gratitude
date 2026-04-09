0f411a6f5ae1db74b35f4cd2db8439a7af847422
microdotsh <8120581+microdotsh@users.noreply.github.com>
2026-04-08 20:08:26 -0500

    Update send_gratitude.sh

--- send_gratitude.sh
+++ send_gratitude.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-MSG_FILE="$HOME/storage/shared/doc/aa/gratitude.txt"
-NUMBER_FILE="$HOME/storage/shared/doc/aa/recipients.txt"
+MSG_FILE="$HOME/storage/shared/doc/gratitude.txt"
+NUMBER_FILE="$HOME/storage/shared/doc/recipients.txt"
 
 LOG="/data/data/com.termux/files/home/sms_debug.log"
 
